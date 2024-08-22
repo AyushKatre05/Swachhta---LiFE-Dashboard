029f475801c9e03ced44f8bc6200f5cc3270db06
Ayushatre05 <ayushkatre1801@gmail.com>
2024-08-22 09:53:29 +0530

    done

--- src/database/mongo.config.js
+++ src/database/mongo.config.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export function connect() {
   mongoose
-    .connect(process.env.MONGO_URL, {tls: true,ssl: true,})
+    .connect(process.env.MONGO_URL, {
+      tls: true,
+      ssl: true,
+    })
     .then(() => console.log("Database connected successfully"))
     .catch((err) => console.log("The DB error is", err));
 }
