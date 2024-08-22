9625a97720629f8a735f9c362d78da11b35ef0af
Ayushatre05 <ayushkatre1801@gmail.com>
2024-08-22 10:38:40 +0530

    changes with routing to hit admin route

--- src/app/api/auth/admin/signup/route.js
+++ src/app/api/auth/admin/signup/route.js
@@ -3,23 +3,18 @@ import { connect } from "@/database/mongo.config";
 import { User } from "@/models/User";
 import bcrypt from "bcryptjs";
 
-// Ensure the database connection is established
 connect();
 
 export async function POST(request) {
-  // Generate salt and hash the password
   const salt = bcrypt.genSaltSync(10);
-  const hashedPassword = bcrypt.hashSync("ak1818", salt);
-
-  // Create a new admin user
+  const password = bcrypt.hashSync("ak1818", salt);
   await User.create({
     email: "ayush@gmail.com",
-    password: hashedPassword,
+    password: password,
     name: "Admin",
     role: "Admin",
   });
 
-  // Return a success response
   return NextResponse.json({
     status: 200,
     message: "Admin created successfully",
