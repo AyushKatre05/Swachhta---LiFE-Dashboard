dff167e28bcbd9bb1c15c66c118b7eb3883e06f3
Ayushatre05 <ayushkatre1801@gmail.com>
2024-08-24 00:45:42 +0530

    added more css

--- src/components/signoutButton.jsx
+++ src/components/signoutButton.jsx
@@ -15,7 +15,8 @@ export default function SignoutButton({ type }) {
           })
         }
       >
-       <LogOut/> 
+      <div className="flex items-center px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-600 rounded-md"><LogOut className="w-5 h-5 mr-3"/> Logout</div>
+       
       </button>
     </div>
   );
