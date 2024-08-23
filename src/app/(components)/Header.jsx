8646310466befe4727537bf259156bc0b0e080a5
Ayushatre05 <ayushkatre1801@gmail.com>
2024-08-24 00:56:56 +0530

    change header name

--- src/app/(components)/Header.jsx
+++ src/app/(components)/Header.jsx
@@ -11,7 +11,7 @@ export default function Header({ toggleSidebar }) {
       >
         <Menu/>
       </button>
-      <h1 className="text-xl">Dashboard Header</h1>
+      <h1 className="text-xl">Swachhta & LiFE Dashboard</h1>
       <ModeToggle/>
     </header>
   );
