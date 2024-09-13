0aa787b88c351fac2a3427c59448bf179c08273d
Ayushatre05 <ayushkatre1801@gmail.com>
2024-09-13 18:12:56 +0530

    made a link for image and video prediction

--- src/app/(pages)/dashboard/statistics/page.jsx
+++ src/app/(pages)/dashboard/statistics/page.jsx
@@ -15,26 +15,28 @@ export default function StatisticsSection() {
       
       {/* Section 1: Daily Statistics and Monthly Trends */}
       <div className="flex flex-col md:flex-row gap-4 md:gap-6">
-        <Link href='/dashboard/analytics'>
+        <Link href='/dashboard/analytics/image'>
         <motion.div
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
           className="bg-gray-100 p-4 md:p-6 rounded-lg shadow-lg flex-1 border border-gray-200 dark:bg-gray-800 dark:border-gray-700"
         >
-          <h3 className="text-lg md:text-xl font-semibold">Daily Statistics</h3>
-          <p>Overview of daily statistics and metrics.</p>
+          <h3 className="text-lg md:text-xl font-semibold">Image Statistics</h3>
+          <p>Overview of image statistics and metrics.</p>
         </motion.div>
         </Link>
+        <Link href={'/dashboard/analytics/video'}>
         <motion.div
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5, delay: 0.2 }}
           className="bg-gray-200 p-4 md:p-6 rounded-lg shadow-lg flex-1 border border-gray-200 dark:bg-gray-700 dark:border-gray-600"
         >
-          <h3 className="text-lg md:text-xl font-semibold">Monthly Trends</h3>
-          <p>Trends and patterns observed over the month.</p>
+          <h3 className="text-lg md:text-xl font-semibold">Video Statistics</h3>
+          <p>Overview of image statistics and metrics.</p>
         </motion.div>
+        </Link>
       </div>
 
       {/* Section 2: Yearly Overview */}
