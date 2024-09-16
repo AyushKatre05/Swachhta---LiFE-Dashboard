acc00951a9f05b174980ad7ad53ecd042952f766
Ayushatre05 <ayushkatre1801@gmail.com>
2024-09-16 16:38:23 +0530

    use client added

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic';
 "use client";
+export const dynamic = 'force-dynamic';
 import React, { useState, useEffect, Suspense } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
--- src/app/(pages)/admin/page.jsx
+++ src/app/(pages)/admin/page.jsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic';
 "use client";
+export const dynamic = 'force-dynamic';
 import React, { Suspense, useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
