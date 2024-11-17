ee6dd39cd932262c709b903b73a52d7746e8db1c
Ayushatre05 <ayushkatre1801@gmail.com>
2024-11-17 19:25:58 +0530

    made new schema for alerts

---
+++ src/models/alert.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+
+const alertSchema = new Schema({
+  message: {
+    type: String,
+    required: [true, "Message is required."],
+  },
+  area: {
+    type: String,
+    required: [true, "Area is required."],
+  },
+  issuedDate: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "In Progress", "Resolved"],
+    default: "Pending",
+  },
+  details: {
+    type: String,
+  },
+  assignedTo: {
+    type: Schema.Types.ObjectId, // Reference to the User (PostMaster)
+    ref: "User",
+    required: true,
+  },
+  updates: [
+    {
+      date: { type: Date, default: Date.now },
+      comment: { type: String },
+    },
+  ],
+});
+
+export const Alert = mongoose.models.Alert || mongoose.model("Alert", alertSchema);
