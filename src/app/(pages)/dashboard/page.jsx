bccf7417eb2bd070d81336aae7ed481587da7b00
Ayush Katre <ayushkatre1801@gmail.com>
2024-12-05 10:54:23 +0530

    removed overview section and made it seperate

--- src/app/(pages)/dashboard/page.jsx
+++ src/app/(pages)/dashboard/page.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import OverviewSection from './OverviewSection/page'
 import ActionsSection from './alert/page'
 import StatisticsSection from './statistics/page'
 
 const Dashboard = () => {
   return (
     <div>
-      <OverviewSection />
       <ActionsSection />
       <StatisticsSection />
     </div>
